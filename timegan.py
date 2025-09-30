e880cd9138fe322bf61c45cdc7aa6a3b7dac1c74
kincaidqf <kfries@oxy.edu>
2025-09-29 17:40:11 -0700

    making empty shell for the timegan implementation

---
+++ timegan.py
@@ -0,0 +1,22 @@
+def timegan(train_set, parameters):
+    # Placeholder for TimeGAN implementation
+
+    def embedder(X, T):
+        return
+    
+    def recovery(H, T):
+        return
+    
+    def generator(Z, T):
+        return
+    
+    def supervisor(H, T):
+        return
+    
+    def discriminator(H, T):
+        return
+    
+    generated_data = list()
+
+    return generated_data
+
