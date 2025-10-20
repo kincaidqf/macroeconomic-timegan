a1634ec5bc1b79ef8bde3822b3a3ef5856f4fa29
kincaidqf <kfries@oxy.edu>
2025-10-20 16:57:59 -0700

    blocking out program for running statistical tests on the data for evaluation

---
+++ eval_metrics.py
@@ -0,0 +1,23 @@
+def load_data(real_path, synth_path):
+    pass
+
+def test_marginals(real, synth):
+    pass
+
+def test_correlation(real, synth):
+    pass
+
+def test_acf(real, synth):
+    pass
+
+def test_discriminative(real, synth):
+    pass
+
+def test_predictive(real, synth):
+    pass
+
+def test_knn_novelty(real, synth):
+    pass
+
+def main():
+    pass
