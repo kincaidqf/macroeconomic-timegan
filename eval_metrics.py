8c85cacc0fedd154cde7907411183ec04fdeb272
kincaidqf <kfries@oxy.edu>
2025-10-29 13:32:24 -0700

    blocking out load_data function

--- eval_metrics.py
+++ eval_metrics.py
@@ -1,6 +1,25 @@
 import numpy as np
 
 def load_data(real_path, synth_path):
+    """
+    Load saved .npy arrays 
+
+    Inputs: 
+        real_path - path to real data .npy file
+        synth_path - path to synthetic data .npy file
+
+    Expected array shape: (N, L, D)
+        N: number of windows
+        L: window length
+        D: feature dimension
+
+    Returns:
+        real - numpy array of real data
+        synth - numpy array of synthetic data  
+    """
+
+    # TODO: load arrays
+    # TODO: check shapes match
     pass
 
 def test_marginals(real, synth):
